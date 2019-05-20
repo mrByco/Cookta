ad31b7c48f031c5a2fb0e0ec1b232dd0cf89624d
Byc0 <matyi0918@gmail.com>
2019-05-20 17:22:45 +0200

    WIP: WeekTemplates

---
+++ Kukta/Menu/Meal.cs
@@ -0,0 +1,19 @@
+﻿using Kukta.FoodFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kukta.Menu
+{
+    class Meal
+    {
+        internal EMealType Type;
+        internal List<FoodCategory> Categories = new List<FoodCategory>();
+        public Meal(EMealType tpye)
+        {
+            Type = Type;
+        }
+    }
+}
