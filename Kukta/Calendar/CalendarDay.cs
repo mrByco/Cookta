427db858698d9cdab798e529f31636551997b4c7
Byc0 <matyi0918@gmail.com>
2019-07-09 20:40:04 +0200

    WIP: Calendar online

--- Kukta/Calendar/CalendarDay.cs
+++ Kukta/Calendar/CalendarDay.cs
@@ -1,6 +1,8 @@
-﻿using Kukta.FrameWork;
+﻿using Kukta.FoodFrameworkV2;
+using Kukta.FrameWork;
 using Kukta.Menu;
 using Kukta.SaveLoad.File;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +13,6 @@ namespace Kukta.Calendar
 {
     public class CalendarDay
     {
-        public string _id;
         public DateTime DateTime;
         public Mealing[] mealings = new Mealing[6];
         public CalendarDay(DateTime dateTime)
@@ -31,6 +32,85 @@ namespace Kukta.Calendar
         public string GetDayString()
         {
             return DateTime.ToString("yyyy-MM-dd");
+        }
+
+        internal static async Task<CalendarDay> GetDay(DateTime dateTime)
+        {
+            var query = new Dictionary<string, object>();
+            query.Add("date", dateTime.ToString("yyyy-MM-dd"));
+            var response = await Networking.GetRequestWithForceAuth("day", query);
+            return await ParseDayFromServerJson(response.Content);
+        }
+
+        private static async Task<CalendarDay> ParseDayFromServerJson(string json)
+        {
+            JObject jFood = JObject.Parse(json);
+            CalendarDay day = new CalendarDay();
+
+            string dateString = jFood.GetValue("date").Value<string>();
+            string format = "yyyy-MM-dd";
+            DateTime date;
+            DateTime.TryParseExact(
+                dateString,
+                format,
+                System.Globalization.CultureInfo.InvariantCulture,
+                System.Globalization.DateTimeStyles.None,
+                out date);
+            day.DateTime = date;
+
+            JArray jarray = jFood.GetValue("mealings").Value<JArray>();
+            if (jarray != null)
+            {
+                for (int i = 0; i < jarray.Count; i++)
+                {
+                    int mealIndex = jarray.ElementAt(i).Value<int>("mealIndex");
+                    string typeString = jarray.ElementAt(i).Value<string>("type");
+                    IMealingItem item;
+                    switch (typeString)
+                    {
+                        case "food":
+                            string foodId = jarray.ElementAt(i).Value<string>("id");
+                            item = await Food.Get(foodId) as IMealingItem;
+                            break;
+                        case "flag":
+                            throw new NotImplementedException();
+                        case "list":
+                            throw new NotImplementedException();
+                        default:
+                            throw new NotImplementedException();
+                    };
+                    day.mealings[mealIndex].items.Add(item);
+                    continue;
+                }
+            }
+            return day;
+        }
+        public static string CreateDayToServer(CalendarDay days)
+        {
+            JObject jFood = new JObject();
+
+            JArray ingArray = new JArray();
+            foreach (Ingredient ing in food.ingredients)
+            {
+                JObject jObject = new JObject();
+                jObject.Add("ingredientID", ing.Type.ID);
+                jObject.Add("unit", ing.UnitName);
+                jObject.Add("value", ing.Value);
+                ingArray.Add(jObject);
+            }
+
+            if (food._id != null)
+            {
+                jFood.Add("_id", JToken.FromObject(food._id));
+            }
+            jFood.Add("name", JToken.FromObject(food.name));
+            jFood.Add("private", JToken.FromObject(food.isPrivate));
+            jFood.Add("makeTime", JToken.FromObject(food.makeTime));
+            jFood.Add("desc", JToken.FromObject(food.desc));
+            jFood.Add("ingredients", ingArray);
+
+            return jFood.ToString(Formatting.None);
+
         }
     }
 }
---
+++ Kukta/Screens/CalendarContentPage.xaml.cs
@@ -0,0 +1,136 @@
+﻿using Kukta.Calendar;
+using Kukta.FoodFrameworkV2;
+using Kukta.FrameWork;
+using Kukta.Menu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Kukta.Screens
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class CalendarContentPage : Page
+    {
+        private Dictionary<EMealType, int> RowByMealType = new Dictionary<EMealType, int>();
+        // -1 is alvays the Mealtypes
+        private Dictionary<int, List<UIElement>> ElementsByDayindex = new Dictionary<int, List<UIElement>>();
+
+        public CalendarContentPage()
+        {
+            this.InitializeComponent();
+            DrawMeals(Enum.GetValues(typeof(EMealType)).Cast<EMealType>().ToList());
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            DateTime startDay = ((DateTime)e.Parameter).StartOfWeek(DayOfWeek.Monday);
+            DrawMeals(Enum.GetValues(typeof(EMealType)).Cast<EMealType>().ToList());
+            for (int i = 0; i < 7; i++)
+            {
+                DrawDay(await CalendarDay.GetDay(startDay.AddDays(i).CutToDay()));
+            }
+        }
+
+
+        private void DrawMeals(List<EMealType> meals)
+        {
+            try
+            {
+                ElementsByDayindex[-1].ForEach((element) => ContentGrid.Children.Remove(element));
+                ElementsByDayindex.Remove(-1);
+            }
+            catch
+            {
+            }
+            ElementsByDayindex.Add(-1, new List<UIElement>());
+            RowByMealType = new Dictionary<EMealType, int>();
+            foreach (EMealType type in meals)
+            {
+                TextBlock TextBlock = new TextBlock();
+                ContentGrid.Children.Add(TextBlock);
+                ElementsByDayindex[-1].Add(TextBlock);
+                TextBlock.Text = type.ToString();
+                TextBlock.VerticalAlignment = VerticalAlignment.Center;
+                TextBlock.HorizontalAlignment = HorizontalAlignment.Center;
+                //Make grid row if not exist
+                if (ContentGrid.RowDefinitions.Count <= meals.Count + 1)
+                {
+                    RowDefinition row = new RowDefinition
+                    {
+                        Height = new GridLength(1, GridUnitType.Auto),
+                        MinHeight = 40.0
+                    };
+                    ContentGrid.RowDefinitions.Add(row);
+                }
+                Grid.SetRow(TextBlock, (int)type + 2);
+                RowByMealType.Add(type, (int)type + 2);
+
+            }
+        }
+
+        private async void DrawDay(CalendarDay day)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                int dayIndex = day.DateTime.DayIndexInWeek(DayOfWeek.Monday) + 1;
+                try
+                {
+                    List<UIElement> elements = ElementsByDayindex[dayIndex];
+                    if (elements != null)
+                        elements.ForEach(e => { ContentGrid.Children.Remove(e); });
+                }
+                catch
+                {
+                }
+                if (ElementsByDayindex.ContainsKey(dayIndex))
+                    ElementsByDayindex[dayIndex] = new List<UIElement>();
+                else
+                    ElementsByDayindex.Add(dayIndex, new List<UIElement>());
+
+                TextBlock dayText = new TextBlock()
+                {
+                    Text = day.DateTime.DayOfWeek.ToString()
+                };
+                ElementsByDayindex[dayIndex].Add(dayText);
+                ContentGrid.Children.Add(dayText);
+                Grid.SetColumn(dayText, dayIndex);
+
+
+
+
+
+                if (day == null)
+                {
+                    ProgressRing ring = new ProgressRing();
+                    ring.IsActive = true;
+                    ElementsByDayindex[day.DateTime.DayIndexInWeek(DayOfWeek.Monday)].Add(ring);
+                    ContentGrid.Children.Add(ring);
+                    return;
+                }
+
+                foreach (EMealType type in RowByMealType.Keys)
+                {
+                    StackPanel foodList = new StackPanel();
+
+                    foreach (Food food in day.GetItemsOf(type))
+                    {
+                        foodList.Children.Add(new TextBlock() { Text = food.name});
+                    }
+                    Grid.SetColumn(foodList, dayIndex);
+                    Grid.SetRow(foodList, RowByMealType[type]);
+                    ContentGrid.Children.Add(foodList);
+                    ElementsByDayindex[dayIndex].Add(foodList);
+                }
+            });
+        }
+    }
+}
