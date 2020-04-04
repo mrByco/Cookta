78e2b3cf9ea55b2733aab010338e11d7c206e584
Byc0 <matyi0918@gmail.com>
2020-04-04 09:56:58 +0200

    Redactor: Meal - Resolving compile errors

--- Cookta-online/src/app/shared/models/menu/mealing.interface.ts
+++ Cookta-online/src/app/shared/models/menu/mealing.interface.ts
@@ -2,6 +2,7 @@ import {EMealType} from './mealtype.enum';
 import {Food} from '../grocery/food.model';
 import {FoodService} from '../../services/food.service';
 import {Injectable} from '@angular/core';
+import {IIngredient} from '../grocery/ingredient.interface';
 
 export interface IMeal {
   type: string;
