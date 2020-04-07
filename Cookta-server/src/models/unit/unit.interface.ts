d7ec824b5840229c853016299a38b21745f3739a
Mátyás Biszterszky <matyi0918@gmail.com>
2020-04-07 19:34:36 +0200

    Constrains fix

--- Cookta-server/src/models/unit/unit.interface.ts
+++ Cookta-server/src/models/unit/unit.interface.ts
@@ -3,7 +3,7 @@ import {EUnitType} from "../../enums/unit-type.enum";
 export interface IUnit {
     type: EUnitType,
     name: string,
-    shortname: string,
+    shortname?: string,
     tobase: number,
     id: string
 
