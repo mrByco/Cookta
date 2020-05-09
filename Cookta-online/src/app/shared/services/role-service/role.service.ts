bd1e503abebaf6cdb32e8f101a26259517da8397
Mátyás Biszterszky <matyi0918@gmail.com>
2020-05-09 11:20:00 +0200

    TEsts dont work

---
+++ Cookta-online/src/app/shared/services/role-service/role.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class RoleService {
+  //public readonly roles: IRole[];
+
+  constructor() { }
+}
--- Cookta-online/src/app/shared/services/shopping-service/shopping-service.service.spec.ts
+++ Cookta-online/src/app/shared/services/shopping-service/shopping-service.service.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ShoppingService } from './shopping.service';
+import {ServerService} from "../server.service";
 
 describe('ShoppingServiceService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+        {provide: ServerService, useValue: {}}
+        ]
+  }));
 
   it('should be created', () => {
     const service: ShoppingService = TestBed.get(ShoppingService);
