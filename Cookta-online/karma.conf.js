443533c6547fae9780af62e0e1ea223b862a195d
Mátyás Biszterszky <matyi0918@gmail.com>
2020-05-09 11:53:44 +0200

    Revert "TEsts dont work"
    This reverts commit bd1e503abebaf6cdb32e8f101a26259517da8397.

--- Cookta-online/karma.conf.js
+++ Cookta-online/karma.conf.js
@@ -13,7 +13,7 @@ module.exports = function (config) {
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      clearContext: true // leave Jasmine Spec Runner output visible in browser
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
       coverageIstanbulReporter: {
           dir: require('path').join(__dirname, './coverage/cookta'),
@@ -24,9 +24,9 @@ module.exports = function (config) {
       port: 9876,
       colors: true,
       logLevel: config.LOG_INFO,
-      autoWatch: false,
+      autoWatch: true,
       browsers: ['Chrome'],
-      singleRun: true,
+      singleRun: false,
       restartOnFileChange: true
   });
 };
--- Cookta-online/src/app/shared/services/shopping-service/shopping-service.service.spec.ts
+++ Cookta-online/src/app/shared/services/shopping-service/shopping-service.service.spec.ts
@@ -1,14 +1,9 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ShoppingService } from './shopping.service';
-import {ServerService} from "../server.service";
 
 describe('ShoppingServiceService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [
-        {provide: ServerService, useValue: {}}
-        ]
-  }));
+  beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
     const service: ShoppingService = TestBed.get(ShoppingService);
