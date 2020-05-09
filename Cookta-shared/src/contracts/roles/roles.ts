f7804e7a5c789c01ca9a82915940efc152cfbf0e
Mátyás Biszterszky <matyi0918@gmail.com>
2020-05-09 08:11:23 +0200

    Created routes

---
+++ Cookta-shared/src/contracts/roles/roles.ts
@@ -0,0 +1,13 @@
+import {IRoute} from "waxen/dist/abstract/route.interface";
+import {ERouteMethod} from "waxen/dist/route-method.enum";
+import {ControllerData} from "waxen/dist/abstract/controller.interface";
+import {IRole} from "../../models/roles/role.interface";
+
+const GetRoles: IRoute<void, IRole[], void> = {method: ERouteMethod.GET, path: ''};
+const SetRole: IRoute<IRole, {success: boolean, edited: IRole}, void> = {method: ERouteMethod.PUT, path: ''};
+const CreateRole: IRoute<IRole, {created: IRole}, void> = {method: ERouteMethod.POST, path: ''};
+const DeleteRole: IRoute<void, {changed: string}, {roleId: string, changeRoleTo: string}> = {method: ERouteMethod.DELETE, path: ''};
+
+export const RoleControllerData: ControllerData = {
+    basepath: "role", name: "Role", routes: [GetRoles]
+}
