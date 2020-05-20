1ec33997a001c8c9e2ce65d499150d9cb7ebe418
Mátyás Biszterszky <matyi0918@gmail.com>
2020-05-20 10:51:13 +0200

    ForceHTTPS Middleware

--- Cookta-server/src/app.ts
+++ Cookta-server/src/app.ts
@@ -2,7 +2,8 @@ import * as express from 'express';
 import * as cors from 'cors';
 import * as bodyParser from "body-parser";
 import * as router from './server/router';
-import {requestLoggerMiddleware} from "./request.logger.middleware";
+import {requestLoggerMiddleware} from "./middleware/request.logger.middleware";
+import {ForceHTTPS} from "./middleware/force-https.middleware";
 
 const fileUpload = require('express-fileupload');
 
@@ -20,11 +21,17 @@ app.use(fileUpload(
 router.RegisterRoutes(app);
 
 
+
+if (process.env.NODE_ENV == "debug "){
+    console.warn('You are running the server in debug mode.')
+    console.warn('Api will available over http.')
+}else{
+    app.use(ForceHTTPS);
+}
+
 app.get('/', (req, res) => {
     res.send('Hello friend! :)');
 });
-
-
 /*if (process.env.NODE_ENV == "debug ") {
     try {
         const swaggerDocument = require('../swagger.json');
