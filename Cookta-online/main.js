b49e5262929cec955c6f3e5c99c3f55955571c8a
Byc0 <matyi0918@gmail.com>
2020-08-04 23:33:58 +0200

    Serve booth rendetron and dynamic, correct bot handling

--- Cookta-online/main.js
+++ Cookta-online/main.js
@@ -1,27 +1,23 @@
-require('zone.js/dist/zone-node');
 const express = require('express');
-const fs = require('fs');
 const url = require('url');
-const fetch = require('node-fetch');
-const join = require('path').join
+const fetchUrl = require('fetch').fetchUrl;
 const ForceHttps = require("./middleware/force-https.middleware");
 require('dotenv').config()
 
 
 const prodUrl = 'https://cookta.me';
-const renderUrl = 'https://render-tron.appspot.com/render';
+const renderUrl = 'https://rendertron-285418.nw.r.appspot.com/render';
 
 
 function generateUrl(request) {
     return url.format({
-        protocol: request.protocol,
         host: prodUrl,
         pathname: request.originalUrl
     })
 }
 
 function detectBot(userAgent) {
-    let test = new RegExp('/bot|crawler|spider|crawling/i');
+    let test = new RegExp(/bot|crawler|spider|crawling/, 'i');
     return test.test(userAgent);
 }
 
@@ -29,26 +25,18 @@ function detectBot(userAgent) {
 function app() {
     const server = express();
 
-    if (process.env.DEBUG === undefined){
+    if (process.env.DEBUG != 'TRUE') {
         server.use(ForceHttps);
     }
-
     server.use((req, res, next) => {
-        let isBot = detectBot(req.headers['User-Agent']);
-        if (isBot) {
+        let isBot = detectBot(req.headers['user-agent']);
+        if (isBot && req.originalUrl !== '/favicon.ico') {
+            const botUrl = generateUrl(req);
+            fetchUrl(`${renderUrl}/${botUrl}`, (error, meta, body) => {
+                res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+                res.set('Vary', 'User-Agent');
 
-            const botUrl = generateUrl(req)
-
-            fetch(`https://cooktaservices.azurewebsites.net/`).then(() => {
-                console.log('fetch');
-                fetch(`${renderUrl}/${botUrl}`)
-                    .then(r => r.text())
-                    .then(body => {
-                        res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
-                        res.set('Vary', 'User-Agent');
-
-                        res.send(body.toString());
-                    });
+                res.send(body.toString());
             });
         } else {
             next();
@@ -56,12 +44,10 @@ function app() {
     });
 
     const public = __dirname + '/dist/cookta/browser';
-
     server.use(express.static(public));
 
-
     server.get('/sitemap.xml', (req, res) => {
-        fetch.fetchUrl('https://cooktaservices.azurewebsites.net/sitemap.xml', (error, meta, body) => {
+        fetchUrl('https://cooktaservices.azurewebsites.net/sitemap.xml', (error, meta, body) => {
             res.send(body);
         })
     });
