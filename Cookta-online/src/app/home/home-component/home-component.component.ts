f7f2444a688e69576e521d1a70c9221dd0926811
Byc0 <matyi0918@gmail.com>
2020-08-08 14:01:53 +0200

    Description and title meta on food pages

--- Cookta-online/src/app/home/home-component/home-component.component.ts
+++ Cookta-online/src/app/home/home-component/home-component.component.ts
@@ -2,6 +2,10 @@ import {Component, OnInit} from '@angular/core';
 import {IHomeContent} from '../../../../../Cookta-shared/src/models/home/home-content.interface';
 import {IHomeRowContent} from '../../../../../Cookta-shared/src/models/home/home-row-content.interface';
 import {HomeService} from '../../shared/services/home.service';
+import {Meta, Title} from '@angular/platform-browser';
+
+// tslint:disable-next-line:max-line-length
+const DESC = 'A legjobb menütervező alkalmazás tele receptekkel, segít kitalálni mit főzz ha nincs ötleted, összeállítja a bevásárló listát, és a boltba is elkísér. A családtagok előre láthatják a menüt, így ők is becsempészhetik kedvenceiket. Receptek, menütervezés, bevásárlő lista, és sok apróság tejlesen ingyen.';
 
 @Component({
   selector: 'app-home-component',
@@ -15,13 +19,16 @@ export class HomeComponentComponent implements OnInit {
   public Rows: IHomeRowContent[];
 
 
-  constructor(public homeService: HomeService) {
+  constructor(public homeService: HomeService, title: Title, meta: Meta) {
+    title.setTitle('Kezdőlap - Cookta');
+    meta.updateTag({name: 'description', content: DESC});
     this.homeService.RefreshStartPage();
   }
 
   ngOnInit(): void {
-    if (this.homeService.HomeContent)
+    if (this.homeService.HomeContent) {
       this.GetRealContent();
+    }
     this.homeService.OnHomeContentChanged.subscribe(() => this.GetRealContent());
   }
 
