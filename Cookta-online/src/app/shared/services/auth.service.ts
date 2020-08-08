24907bc60d2b66b016f2ff4251882b4636521530
matyi <matyi0918@gmail.com>
2020-08-08 09:23:42 +0200

    Auth service refactored

--- Cookta-online/src/app/shared/services/auth.service.ts
+++ Cookta-online/src/app/shared/services/auth.service.ts
@@ -6,42 +6,46 @@ import {EventEmitter, Injectable} from '@angular/core';
 export class AuthService {
     loggedIn: boolean;
     readonly OnUserLoginChanged: EventEmitter<void> = new EventEmitter<void>();
-    public get CurrentAuthMethod(){
-        return 'google-oauth2';
-    }
 
-    public get IsAuthenticated(): boolean | Promise<boolean>{
-        return this.GoogleUser?.isSignedIn() ?? (this.GoogleAuth as Promise<any>).then((a) => {
-            a.isSignedIn.get();
-        })
-    };
-    public async getTokenSilently(): Promise<string>{
-        if (!this.GoogleUser || !await this.IsAuthenticated){
-            return;
-        }
-        let response = this.GoogleAuth.currentUser.get().getAuthResponse(true)
-        return response.id_token;
-    }
+    private Gapi: any;
+    private GoogleAuth: any | Promise<any>;
+    private GoogleUser: any;
+    private resolveGoogleAuth: (gapi: any) => void;
 
     constructor() {
         this.GoogleAuth = new Promise<any>((r) => {
             this.resolveGoogleAuth = r;
-        })
+        });
 
     }
 
-    private Gapi: any;
-    private GoogleAuth: any | Promise<any>
-    private GoogleUser: any;
-    private resolveGoogleAuth: (gapi: any) => void;
+    public get CurrentAuthMethod() {
+        return 'google-oauth2';
+    }
 
-    InitGapiAuth(gapi: any){
-        gapi.auth2.init({client_id: '186738034909-in3efiaqeebaej9njsep88f3ejqu78r9.apps.googleusercontent.com'})
-        let instance = gapi.auth2.getAuthInstance();
+    public get IsAuthenticated(): boolean | Promise<boolean> {
+        return this.GoogleUser?.isSignedIn() ?? (this.GoogleAuth as Promise<any>).then((a) => {
+            a.isSignedIn.get();
+        });
+    }
+
+    public async getTokenSilently(): Promise<string> {
+        if (!this.GoogleUser || !await this.IsAuthenticated) {
+            return;
+        }
+        const response = this.GoogleAuth.currentUser.get().getAuthResponse(true);
+        return response.id_token;
+    }
+
+    InitGapiAuth(gapi: any) {
+        gapi.auth2.init({client_id: '186738034909-in3efiaqeebaej9njsep88f3ejqu78r9.apps.googleusercontent.com'});
+        const instance = gapi.auth2.getAuthInstance();
         this.resolveGoogleAuth(instance);
         this.GoogleAuth = instance;
-        this.GoogleAuth.isSignedIn.listen(() => {this.UserLoginChanged();});
-        if (this.GoogleAuth.currentUser.get()){
+        this.GoogleAuth.isSignedIn.listen(() => {
+            this.UserLoginChanged();
+        });
+        if (this.GoogleAuth.currentUser.get()) {
             this.PostLogin();
         }
         this.Gapi = gapi;
@@ -57,14 +61,16 @@ export class AuthService {
         this.GoogleAuth.disconnect();
         location.reload();
     }
+
     async Login() {
-        let auth = await this.GoogleAuth;
+        const auth = await this.GoogleAuth;
         await auth.signIn({prompt: 'select_account'});
         this.PostLogin();
     }
-    PostLogin(){
+
+    PostLogin() {
         this.GoogleUser = this.GoogleUser || this.GoogleAuth.currentUser.get();
-        this.loggedIn = this.GoogleUser?.isSignedIn()?? false;
+        this.loggedIn = this.GoogleUser?.isSignedIn() ?? false;
         this.OnUserLoginChanged.emit();
     }
 }
