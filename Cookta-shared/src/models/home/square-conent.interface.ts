3bd152667b49b10220cfdca0d0cb36e616ae6a48
Byc0 <matyi0918@gmail.com>
2020-09-13 15:11:41 +0200

    Home square click content works

--- Cookta-shared/src/models/home/square-conent.interface.ts
+++ Cookta-shared/src/models/home/square-conent.interface.ts
@@ -1,5 +1,5 @@
 export interface ISquareContent {
-    images: {url: string, subtitle: string}[],
+    images: {url: string, subtitle: string, foodId: string}[],
     title: string,
     clickAction: string
 }
